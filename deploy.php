b37ab448f8ee70d961c75f7d63fda4dcd3e2dd28
mike.shen <mike.shen@atommatrix.com>
2018-10-25 16:20:04 +0800

    test

---
+++ deploy.php
@@ -0,0 +1,43 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'git@github.com:TwlSweet/laratest.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('project.com')
+    ->user('deployer') // 这里填写 deployer
+    // 并指定公钥的位置
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/laratest');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
